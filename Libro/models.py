80a1f35f59ec3e563111687023b4c50839f83f38
Jesus_Adrian <jesusadrianmamanicondo2@gmail.com>
2025-06-07 18:36:19 -0400

    creacion del models.py de la tabla libro

--- Libro/models.py
+++ Libro/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Libro(models.Model):
+    libro_id = models.AutoField(primary_key=True)
+    titulo = models.CharField(max_length=100)
+    autor = models.CharField(max_length=100)
+    editorial = models.CharField(max_length=100)
+    disponibilidad = models.BooleanField(default=True)
+    codigo = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.titulo} - {self.autor}"

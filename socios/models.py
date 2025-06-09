6fb28c520b5dd097102641eaffacf42fad6a274b
YsraelStyle <israelarismendi5@gmail.com>
2025-06-09 00:32:06 -0400

    cambios

--- socios/models.py
+++ socios/models.py
@@ -1,14 +1,13 @@
 from django.db import models
 
-
 class Socio(models.Model):
-    id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=50)
     ci = models.CharField(max_length=20)
     direccion = models.CharField(max_length=50)
     correo = models.EmailField(unique=True)
-    contrasena = models.CharField(max_length=50)
+    contraseña = models.CharField(max_length=50)
     celular = models.CharField(max_length=20)
+    
 
     def __str__(self):
-        return f"{self.nombre} (CI: {self.ci}, Email: {self.correo})"
+        return self.nombre

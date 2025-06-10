3fcf2a093daa04e0b53db4039cdbd2588d3b70a9
Jesus_Adrian <jesusadrianmamanicondo2@gmail.com>
2025-06-09 22:18:05 -0400

    creacion la opcion de eliminar de la tabla libro

--- Libro/urls.py
+++ Libro/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("detalle/<int:id>/", views.detalle_libro, name="detalle_libro"),
     path("crear/", views.crear_libro, name="crear_libro"),
     path("editar/<int:id>/", views.editar_libro, name="editar_libro"),
-    path("eliminar/<int:id>/", views.eliminar_libro, name="eliminar_libro"),
+   path('libros/eliminar/<int:id>/', views.eliminar_libro, name='eliminar_libro'),
+
 ]

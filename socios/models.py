06b7d1dcb48b7b98ce9524d7d1ac597defe4d7a8
YsraelStyle <israelarismendi5@gmail.com>
2025-06-16 12:05:14 -0400

    modifique tabla socios

--- socios/models.py
+++ socios/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
 class Socio(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
-    ci = models.CharField(max_length=20)
-    direccion = models.TextField(blank=True)
-    celular = models.CharField(max_length=20)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    ci = models.CharField("Carnet de Identidad", max_length=20)
+    direccion = models.TextField("Dirección", blank=True)
+    celular = models.CharField("Celular", max_length=20)
 
     def __str__(self):
-        return self.user.get_full_name()
+        return f"{self.user.first_name} {self.user.last_name}"
+

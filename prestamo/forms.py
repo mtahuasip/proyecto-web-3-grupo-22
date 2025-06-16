66e5269c450da3cfcf797d6f56d4641ec3fc8927
ADamelX <damarisaquise@gmail.com>
2025-06-16 03:40:44 -0400

    modificacion en app prestamo en el forms para seleccionar los datos de socio y de libro

--- prestamo/forms.py
+++ prestamo/forms.py
@@ -4,45 +4,34 @@ from Libro.models import Libro
 from socios.models import Socio
 
 class PrestamoForm(forms.ModelForm):
-    libro_id = forms.IntegerField(
-        label="ID del Libro", 
-        widget=forms.NumberInput(attrs={'class': 'form-control'})
+    libro = forms.ModelChoiceField(
+        queryset=Libro.objects.all(),
+        label="Libro",
+        widget=forms.Select(attrs={'class': 'form-control'})
     )
-    socio_id = forms.IntegerField(
-        label="ID del Socio", 
-        widget=forms.NumberInput(attrs={'class': 'form-control'})
+
+    socio = forms.ModelChoiceField(
+        queryset=Socio.objects.all(),
+        label="Socio",
+        widget=forms.Select(attrs={'class': 'form-control'})
     )
-    
+
     class Meta:
         model = Prestamo
-        fields = ['libro_id', 'socio_id', 'fecha_limite']
-        widgets = {
-            'fecha_limite': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
-        }
-        labels = {
-            'fecha_limite': 'Fecha Límite de Devolución',
-        }
-    
-    def clean_libro_id(self):
-        libro_id = self.cleaned_data.get('libro_id')
-        try:
-            libro = Libro.objects.get(pk=libro_id)
-        except Libro.DoesNotExist:
-            raise forms.ValidationError("No existe un libro con ese ID.")
-        return libro  
+        fields = ['libro', 'socio', 'fecha_limite']
+        widgets = {'fecha_limite': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),}
+        labels = {'fecha_limite': 'Fecha Límite de Devolución',}
 
-    def clean_socio_id(self):
-        socio_id = self.cleaned_data.get('socio_id')
-        try:
-            socio = Socio.objects.get(pk=socio_id)
-        except Socio.DoesNotExist:
-            raise forms.ValidationError("No existe un socio con ese ID.")
-        return socio
+    def clean_fecha_limite(self):
+        fecha_limite = self.cleaned_data.get('fecha_limite')
+        if fecha_limite is None:
+            raise forms.ValidationError("Debes ingresar una fecha límite de devolución.")
+        return fecha_limite
 
     def save(self, commit=True):
         prestamo = super().save(commit=False)
-        prestamo.libro = self.cleaned_data['libro_id']
-        prestamo.socio = self.cleaned_data['socio_id']
+        prestamo.libro = self.cleaned_data['libro']
+        prestamo.socio = self.cleaned_data['socio']
         if commit:
             prestamo.save()
         return prestamo
